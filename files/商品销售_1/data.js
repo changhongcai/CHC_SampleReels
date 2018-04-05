65037afb3ee0c42a5e790270bb662f2ecb65dac7
changhongcai <410289225@qq.com>
2018-04-05 13:57:09 +0800

    resume
    resume and sample reels

---
+++ files/商品销售_1/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G],H,_(I,J,K,L,M,N,O,_(),P,_(Q,R,S,T,U,_(V,W,X,Y),Z,null,ba,T,bb,T,bc,bd,be,null,bf,bg,bh,bi,bj,bk,bl,bg),bm,_(),bn,_(),bo,_(bp,[])),bq,_(),br,_());}; 
+var b="url",c="商品销售_1.html",d="generationDate",e=new Date(1522907597364.02),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="one",o="daohang",p="UpdateInstructions",q="Selected_functions",r="Quick_configuration",s="InstallationStep",t="SecuritySwitch",u="ClientInstallStep",v="GuideSwitch",w="SuperMode",x="Back",y="newType",z="dev",A="which",B="quanxian",C="butie",D="duanxin",E="minge",F="moreorless",G="chooseBar",H="page",I="packageId",J="28296fcc53af41d489f26cdad2801c75",K="type",L="Axure:Page",M="name",N="商品销售",O="notes",P="style",Q="baseStyle",R="627587b6038d43cca051c114ac41ad32",S="pageAlignment",T="near",U="fill",V="fillType",W="solid",X="color",Y=0xFFFFFFFF,Z="image",ba="imageHorizontalAlignment",bb="imageVerticalAlignment",bc="imageRepeat",bd="auto",be="favicon",bf="sketchFactor",bg="0",bh="colorStyle",bi="appliedColor",bj="fontName",bk="Applied Font",bl="borderWidth",bm="adaptiveStyles",bn="interactionMap",bo="diagram",bp="objects",bq="masters",br="objectPaths";
+return _creator();
+})());
